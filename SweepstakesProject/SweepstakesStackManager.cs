1ea79cc135c9029c1e9f9552162f3ac408a97737
unknown <christinesoto21@gmail.com>
2020-03-02 16:55:43 -0600

    Layout in progress

--- SweepstakesProject/SweepstakesStackManager.cs
+++ SweepstakesProject/SweepstakesStackManager.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class SweepstakesStackManager
+    class SweepstakesStackManager : ISweepstakesManager
+    //*******USe Stack Data Structure as an underlying structure**************
+    //Inherit ISweepstakesManager and implement the methods from the ISweepstakesManager interface using Stack and Queue methods.
     {
+        // Member Variables
+        private Stack<Sweepstakes> stack;
+
+        //Constructor
+
+
+        //Member Methods
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+        public Sweepstakes GetSweepstakes()
+        {
+
+        }
+        void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        Sweepstakes GetSweepstakes()
+        {
+
+        }
+
     }
 }
